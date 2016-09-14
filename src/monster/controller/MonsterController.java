ceacc78150139c2e30a6a538fea73629bb5f0758
kmart22 <kmar7070@740s-pl-111.local>
2016-09-14 09:19:25 -0600

    added start method to monster controller

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,23 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController
 {
+	private MarshmallowMonster firstMonster;
+	
+	public MonsterController()
+	{
+		firstMonster = new MarshmallowMonster("gloober", 2.5, 3, true, 2, 1);
+		
+		
+		
+		
+		
+	}
 
+	public void start()
+	{
+		System.out.println("here is my monster: " + firstMonster);
+	}
 }
